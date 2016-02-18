bb85179017e151ade6cfe38c0e45903ca917cf20
Thomas Beekman <beekman.thomas@kpmg.nl>
2016-02-18 13:29:09 +0100

    Initial commit

---
+++ NVA-DotNetReferenceImplementation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NVA_DotNetReferenceImplementation.Startup))]
+namespace NVA_DotNetReferenceImplementation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
